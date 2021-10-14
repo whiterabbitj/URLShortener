126b51e1abbe18c823ec2e11c387ff7c4d1d8f3c
Tomas Sukevicius <tomas.sukevicius@gabarnie.co.uk>
2021-10-14 15:48:35 +0100

    check for empty string

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -29,25 +29,33 @@ namespace MainWebsite
             var urlResult = Request.Form["TxtSubmitUrl"];
 
             //check if proper URL 
-            if (GetResponseBody(urlResult) && !string.IsNullOrEmpty(urlResult))
+           if(string.IsNullOrEmpty(urlResult))
             {
-                //post Data
-                da.AsyncApiCall(urlResult, Endpoint).Wait();
-            
-                //Post result to ui 
-                TXTUrlOutput.Text = "Your New URL - " + da.SUrl;
-                TXTUrlOutput.ForeColor = Color.FromArgb(176, 202, 199);
-                ccButton.Visible = true;
-                clipboard.Visible = true;
+                 if (GetResponseBody(urlResult))
+                 {
+                     //post Data
+                    da.AsyncApiCall(urlResult, Endpoint).Wait();
+                    //Post result to ui 
+                    TXTUrlOutput.Text = "Your New URL - " + da.SUrl;
+                    TXTUrlOutput.ForeColor = Color.FromArgb(176, 202, 199);
+                    ccButton.Visible = true;
+                    clipboard.Visible = true;
+                 }
+                 else
+                     ShowException();
+
             }
             else
-            {
+               ShowException();
+
+        }
+
+        public void ShowException()
+        {
                 TXTUrlOutput.Text = "You have Entered a Wrong URL";
                 TXTUrlOutput.ForeColor = Color.FromArgb(255, 82, 28);
                 ccButton.Visible = true;
                 clipboard.Visible = false;
-            }
-
         }
 
         private bool GetResponseBody(string url)
